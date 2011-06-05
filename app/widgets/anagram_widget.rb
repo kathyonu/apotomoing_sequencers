f1817ece9aa7d06f9fdb4b83517d0a3fc348eb54
kathyonu <iam@goodworksonearth.org>
2011-06-04 23:15:23 -0700

    anagram entry is working, update display is not

--- app/widgets/anagram_widget.rb
+++ app/widgets/anagram_widget.rb
@@ -2,10 +2,7 @@ class AnagramWidget < Apotomo::Widget
   helper ApplicationHelper
 
   responds_to_event :submit, :from => :anagram, :with => :submit, :on => :anagram, :passing => :root
-
-  #responds_to_event :submit, :from => :sequence_created, :with => :submit, :on => :anagram, :passing => :root
-  #responds_to_event :submit, :from => :dashboard, :with => :submit, :on => :anagram, :passing => :root
-  #responds_to_event :submit, :from => :sequence_created, :with => :submit, :on => :anagram, :passing => :root   ### moved to anagram
+  responds_to_event :newAnagram, :with => :update
 
   def anagram
     @anagram = Anagram.new
@@ -29,13 +26,19 @@ class AnagramWidget < Apotomo::Widget
   
   def submit(evt)
     anagram = Anagram.new(:anagram_text => evt[:anagram_text], :description => evt[:description], :reference => evt[:reference]).save
-    @anagrams = Anagram.find(:all)
-    #@anagram = Anagram.find(evt[:id])
     #@anagram.update_attributes(evt[:anagram])
+
+	@anagrams = Anagram.find(:all)
     #anagram = Anagram.new(:id => evt[:id], :text => evt[:text], :sequence_created_id => evt[:sequence_created_id], :description => evt[:description], :reference => evt[:reference]).save
-    #trigger :newSequenceCreated
+    trigger :newAnagram
+
+    update :state => :display
+  end
+
+
+  def update(evt)
 
     replace :state => :display
   end
-
+  
 end
