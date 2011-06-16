684cc19ca259c8aaad1e54a0ed4790da262b2704
kathyonu <iam@goodworksonearth.org>
2011-06-15 21:25:58 -0700

    restored fivesequencers.js file as examples in progress

--- public/javascripts/fivesequencers.js
+++ public/javascripts/fivesequencers.js
@@ -1,7 +1,110 @@
+/* not in use, except to hold examples
+
+window.addEventListener(
+  'load',
+  function() {
+      document.getElementById('input#anagram_text').addEventListener(
+        'focus',
+         function() {
+            if (this.value == "Enter Data") {
+              this.value = "";
+            }
+          }, false
+      );
+
+      document.getElementById('input#anagram_text').addEventListener(
+        'blur',
+         function() {
+          if (!this.value) {
+              this.value = 'poooo';
+          }
+        }, false
+      );
+  }, false
+);
+
+
+
+
+==========================================================================================================================================================================================================
+
+
+
+
+
+
+
+$j(document).ready(function() {
+  var element = $j('#anagram form input#anagram_text');
+  element.addEventListener('focusout',function(e) {
+    var anagramText = $this.value();
+    var output = $j('.formSequenceCreated#sequenceCreated form input#sequence_text').text();
+     anagramText.form.onsubmit = function() { return false; }
+      function() {
+        var text = (anagramText.value.split('').sort()).toString();
+        output.textContent = text;
+      };
+  }, false);
+  element.addEventListener('focusout',function(event) {
+    var anagramText = $this.value();
+    var output = $j('.formSequenceCreated#sequenceCreated form input#sequence_creation').text();
+      anagramText.form.onsubmit = function() { return false; }
+        function() {
+          var creation = (anagramText.value.split('').sort()).toString();
+          output.textContent = creation;
+        };
+  }, false);
+  element.addEventListener('focusout',function(event) {
+    var anagramText = $this.value();
+    var output = $j('.formSequenceCreated#sequenceCreated form input#sequence_complete').text();
+      anagramText.form.onsubmit = function() { return false; }
+        function() {
+          var complete = (anagramText.value.split('').sort()).toString();
+          output.textContent = complete;
+        };
+  }, false);
+  element.addEventListener('focusout',function(event) {
+    var anagramText = $this.value();
+    var output = $j('.formSequenceCreated#sequenceCreated form input#sequence_lexigram').text();
+    anagramText.form.onsubmit = function() { return false; }
+      function() {
+        var lexigram = (anagramText.value.split('').sort()).toString();
+        output.textContent = lexigram;
+      };
+  },false);
+  element.addEventListener('focusout',function(event) {
+    var anagramText = $this.value();
+    var output = $j('.formSequenceCreated#sequenceCreated form input#sequence_singular').text();
+    anagramText.form.onsubmit = function() { return false; }
+      function() {
+        var singular = (anagramText.value.split('').sort()).toString();
+        output.textContent = singular;
+      };
+  },false);
+  var submit = document.getElementById('formAnagram input#submit');
+  submit.parentNode.removeChild(submit);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+another option >
+
+====================================================
 
 
   <%= javascript_include_tag "fivesequencers.js" %>
-w
+---------------------------------------------------
 
 $j(document).ready(
   function() {
@@ -17,3 +120,79 @@ $j(document).ready(
     });
   }
 );
+
+
+
+another option
+--------------------------------------------------
+
+
+$j(document).ready(
+  function() {
+    $j('.formanagram#anagram form input#anagram_text').bind(
+      'onblur', 
+      function() {
+        var anagramText = $j.this.value();
+        var output = $j('.formSequenceCreated#sequenceCreated form input#sequence_text').text();
+        // output.textContent = 'Type your entry, it will be sequenced right then!';
+        // sequenceCreated.form.appendChild(output);
+        anagramText.form.onsubmit = function() { return false; }
+          function() {
+            var text = (anagramText.value.split('').sort()).toString();
+            output.textContent = text;
+          };
+        //var submit = document.getElementById('submit');
+        //submit.parentNode.removeChild(submit);
+      },
+      function() {
+        var anagramText = this.value();
+        var output = $j('.formSequenceCreated#sequenceCreated form input#sequence_creation').text();
+        // output.textContent = 'Type your entry, it will be sequenced right then!';
+        anagramText.form.onsubmit = function() { return false; }
+          function() {
+            var creation = (anagramText.value.split('').sort()).toString();
+            output.textContent = creation;
+          };
+        //var submit = document.getElementById('submit');
+        //submit.parentNode.removeChild(submit);
+      },
+      function() {
+        var anagramText = this.value();
+        var output = $j('.formSequenceCreated#sequenceCreated form input#sequence_complete').text();
+        // output.textContent = 'Type your entry, it will be sequenced right then!';
+        anagramText.form.onsubmit = function() { return false; }
+          function() {
+            var complete = (anagramText.value.split('').sort()).toString();
+            output.textContent = complete;
+          };
+        //var submit = document.getElementById('submit');
+        //submit.parentNode.removeChild(submit);
+      },
+      function() {
+        var anagramText = this.value();
+        var output = $j('.formSequenceCreated#sequenceCreated form input#sequence_lexigram').text();
+        // output.textContent = 'Type your entry, it will be sequenced right then!';
+        anagramText.form.onsubmit = function() { return false; }
+          function() {
+            var lexigram = (anagramText.value.split('').sort()).toString();
+            output.textContent = lexigram;
+          };
+        //var submit = document.getElementById('submit');
+        //submit.parentNode.removeChild(submit);
+      },
+      function() {
+        var anagramText = this.value();
+        var output = $j('.formSequenceCreated#sequenceCreated form input#sequence_singular').text();
+        // output.textContent = 'Type your entry, it will be sequenced right then!';
+        anagramText.form.onsubmit = function() { return false; }
+          function() {
+            var singular = (anagramText.value.split('').sort()).toString();
+            output.textContent = singular;
+          };
+        var submit = document.getElementById('submit');
+        submit.parentNode.removeChild(submit);
+      });
+    });
+*/
+
+
