5631bf0fd7656b2d72d5f70bbe49928493aa87e7
kathyonu <iam@goodworksonearth.org>
2011-07-08 23:39:15 -0700

    fixed open and close button placements

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$j(document).ready(
+  function() {
+    $j('#anagram form input#submit').hide();
+  });
+  
--- public/javascripts/opensequencer.js
+++ public/javascripts/opensequencer.js
@@ -2,7 +2,6 @@ $j(document).ready(
   function() {
     $j('input#sequencerOpen').click(
       function($je) {
-        //$j('#anagram form input#submit').hide();
         $j('.sequenceCreatedWidgetDisplay').show();
         $j('#anagramListDisplay').hide();
         $j('#sequencerWidgetDisplay').show();  /* to keep distance below bottom of display for the open sequencer widget button */
