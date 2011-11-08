25786b6a8990b1d7560c5767e5b71f9e10146a9b
kathyonu <iam@goodworksonearth.org>
2011-11-07 16:31:42 -0800

    removed notes and such from helpers

--- app/helpers/sequence_createds_helper.rb
+++ app/helpers/sequence_createds_helper.rb
@@ -1,13 +1,4 @@
 module SequenceCreatedsHelper
   include ApplicationHelper
 
-# include Histogram
-# include Lexigram
-# include Textual
-
-  # TODO what is the purpose of this code ? is it used ? is it tested ? what does it affect ? 
-#  def sequence_lexigram(sequencetext)
-#    @sequence_lexigram = lexigram_sequencer(sequencetext)
-#  end
-  
 end
