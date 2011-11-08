da3577bbae19d6486a7517d1d30a371c94896dee
kathyonu <iam@goodworksonearth.org>
2011-11-07 16:32:48 -0800

    removed unneeded code form sequence model

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -2,8 +2,4 @@ class Sequence < ActiveRecord::Base
 
   attr_accessible :id, :sequence_text, :sequence_creation, :sequence_complete, :sequence_lexigram, :sequence_singular, :description, :reference, :anagram, :name, :phrase, :sexualities, :external, :internal
 
-#  self.extend(Lexigram)
-#  self.extend(Textual)
-#  self.extend(Histogram)
-  
 end
