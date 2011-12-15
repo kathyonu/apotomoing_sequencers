cdeb4f5f8138bbe9ebb271cb7ebe9bcc56d934fd
kathyonu <iam@goodworksonearth.org>
2011-12-14 17:45:43 -0800

    removed deleted files

--- db/migrate/20110629000007_change_sequences_sequence_created_name.rb
+++
@@ -1,9 +0,0 @@
-class ChangeSequencesSequenceCreatedName < ActiveRecord::Migration
-  def self.up
-    rename_column :sequences, :sequence_created, :sequence_text
-  end
-
-  def self.down
-    rename_column :sequences, :sequence_text, :sequence_created
-  end
-end
--- db/migrate/20111206002442_add_lense_column.rb
+++
@@ -1,9 +0,0 @@
-class AddLenseColumn < ActiveRecord::Migration
-  def self.up
-    add_column :sequences, :lense, :string, :limit => 75
-  end
-
-  def self.down
-    remove_column :sequences, :lense, :string
-  end
-end
