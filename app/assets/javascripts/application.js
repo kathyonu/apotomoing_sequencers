4388d092a3e4b2996c3baf6f4ea46560bae4480c
Will Schive <lexigram@goodworksonearth.org>
2012-09-04 09:54:19 -0700

    javascript change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,6 @@
 //= require jquery
 //= require jquery_ujs
-//= require_self
 //= require almost_ready.js
-//- require jquery-1.6.4.js
 //= require jquery.activity-indicator-1.0.0.min.js
 //= require search_complete.js
 //= require search_creation.js
