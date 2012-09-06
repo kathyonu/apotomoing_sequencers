f8449b67047c6c33782cb9430ae983e486eb6121
Will Schive <lexigram@goodworksonearth.org>
2012-09-05 23:40:29 -0700

    rails 3.2.8

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,5 +27,5 @@ ApotomoingSequencers::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 end
