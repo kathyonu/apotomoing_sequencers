e74710cb09c7d98bc07ae52d0b29ddf4a55225c7
Will Schive <lexigram@goodworksonearth.org>
2012-09-12 12:06:59 -0700

    removed files

--- app/assets/javascripts/search_lexigramss_focusin.js
+++
@@ -1,7 +0,0 @@
-// FOCUSIN on '#search_sequence_lexigrams form input#sequencetext'
-$(document).ready(  function() {
-  $('#search_sequence_lexigrams form input#sequencetext').bind(
-    'focusin',function(event) {    
-      $('#search_sequence_lexigrams form input#sequencetext').val("")
-    })
-  });
