5a14baa45232c8ecf733d2adf44927887de72427
Will Schive <lexigram@goodworksonearth.org>
2012-09-17 21:26:20 -0700

    file removal

--- app/assets/javascripts/search_texts_focusin.js
+++
@@ -1,7 +0,0 @@
-// FOCUSIN on '#search_sequence_texts form input#sequencetext'
-$(document).ready(  function() {
-  $('#search_sequence_texts form input#sequencetext').bind(
-    'focusin',function(event) {    
-      $('#search_sequence_texts form input#sequencetext').val("")
-    })
-  });
